2d2acd8d0918d0be46bb20f67fd30c2d6c6cd0d4
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2013-10-27 14:38:21 -0400

    Started adding documentation

--- readme.md
+++ readme.md
@@ -1,9 +1,59 @@
-Ideas:
-- Yahoo Answers does provide API for answers given a question ID, but since we're only allowed 5000 API requests per day, this would be insufficient. Maybe request more from Yahoo.
-- Probably throw this into MySQL database, dump and send
+#Yahoo Answers Scraper
 
-Issues:
+This python script is for fetching data about questions and answers from [Yahoo Answers](http://answers.yahoo.com). It uses the [Yahoo Answers API](http://developer.yahoo.com/answers/) to fetch questions from a single category at a time. 
+
+##Prerequisites
+You need the Python libraries [BeautifulSoup](http://www.crummy.com/software/BeautifulSoup/) as well as [MySQLDB](http://sourceforge.net/projects/mysql-python/)
+
+##Usage
+###Summary
+
+```
+python scrape.py -get=[questions|answers] -category=["Business & Finance"|"Health"|"Travel"|"Sports"|"Home & Garden"|"Entertainment & Music"|<custom category>]
+```
+
+###Fetching Questions
+Example for fetching questions from "Sports" category
+
+```
+python scrape.py -get=questions -category="Sports"
+```
+
+This will write all of the questions to the file: data/Sports.json
+
+###Fetching Answers
+Example for fetching answers from "Sports" category (assuming you have already fetched the questions)
+
+```
+python scrape.py -get=answers -category="Sports"
+```
+
+This will read all of the questions from data/Sports.json and write to your MySQL database both the questions and answers.
+
+##Errors
+Sometimes Yahoo Answers returns duplicate question ids causing MySQL errors on insertion. These errors are written in the errors.txt file, but should not effect the running of the program since the duplicates are ignored.
+
+##Data Structure
+TODO
+
+##Implementation
+It seems that the YQL table for the Yahoo Answers data only allows 50 questions to be returned at a single time. Therefore, this script loops to fetch 50 questions at a time until your desired amount is reached. This is fetched all into the data directory. When fetching questions, the data will go into:
+
+'''
+data/<category name>.json
+'''
+
+The reason that the questions are stored locally is to reduce the API calls to Yahoo since they are limited. This way, once you fetch the question data, you do not have to call the API again.
+
+Once questions are stored the next thing to do is fetch the answers.
+
+<hr/>
+##Ideas:
+- Yahoo Answers does provide API for answers given a question ID, but since we're only allowed 5000 API requests per day, this would be insufficient. Maybe request more from Yahoo.
+
+##Issues:
 - There doesn't seem to be a way to query for questions with more than 5 answers. This means we will probably have to go through many questions to find 1000 with 5 or more answers.
 - Yahoo only shows downvotes when you sign in
 - When fetching answers, I found a few duplicates in the question ids... so for now I modified mysql errors to write to a file instead of exiting as to not interrupt the program. Question id's are: 20131020103951AAJqaTE, 20131016175727AAa1l43, 20131015174409AAImTMz
 - chosen_answer_timestamp and chosen_answer_award_timestamp did not get filled
+- It seems like after so many questions are fetched, you get blank results back
---
+++ scrape.py
@@ -0,0 +1,214 @@
+#!/usr/bin/python
+
+#I would advise doing the fetching in parallel, and possibly even the parsing (I'm sure that MySQL would be fine with all of the concurrent insertions, but still, it's probably better to parse one at a time and make sure the data looks okay)
+
+import json
+import httplib, urllib
+from bs4 import BeautifulSoup
+import re
+import _mysql
+import sys
+from time import gmtime, strftime
+
+
+#config globals
+numQuestions = 12000 #number of questions desired from each category
+numAllowed = 50 #number of results allowed by a single request (do not increase unless YQL starts allowing more than 50 to be returned per request)
+
+HOST = "localhost"
+USER = "root"
+PW = "102533"
+DB = "ya"
+
+catIDs = {}
+catIDs["Business & Finance"] = 396545013
+catIDs["Health"] = 396545018
+catIDs["Travel"] = 396545469
+catIDs["Sports"] = 396545213
+catIDs["Home & Garden"] = 396545394
+catIDs["Entertainment & Music" = 396545016
+
+'''
+Current Categories:
+
++ Business & Finance
++ Health
++ Travel
++ Sports
++ Home & Garden
++ Entertainment & Music
+
+You can add any other category you want by adding the the catIDs dictionary like so:
+
+catIDs[<category name>] = <category id>
+
+I found the category ids from just going to the category on the Yahoo answers website, the category id is in the url under the sid parameter
+E.g. Going to the health category takes you here: http://answers.yahoo.com/dir/index?sid=396545018
+Also useful to double check is using the online YQL console they provide: http://developer.yahoo.com/yql/console
+
+'''
+
+
+#fetches data from each category
+def fetchData(cat):
+	global numQuestions, numAllowed, catIDs
+	print "Fetching from %s" % cat
+	fname = "data/%s.json" % cat
+	f = open(fname, 'w')
+	f.write("[")
+	first = True
+	numSteps = int(numQuestions / numAllowed) #max of 50 can be retrieved from public API (or so I think)
+	conn = httplib.HTTPConnection("query.yahooapis.com:80")
+	for i in range(numSteps):
+		s = "select * from answers.getbycategory(%d, %d) where category_id=%s" % (i * numAllowed, numAllowed, catIDs[cat])
+		s = urllib.quote(s)
+		conn.request("GET", "/v1/public/yql?q=%s&format=json&sort=ans_count_desc" % s)
+		r = conn.getresponse()
+		data = r.read()
+		if not first:
+			f.write(",")
+		first = False
+		f.write(data)
+		print "%d of %d completed" % (i+1, numSteps)
+	f.write("]")
+	f.close()
+	conn.close()
+	print "File %s is complete" % fname 
+
+jObj = [] #tmp
+soup = []
+def parseData(cat):
+	global jObj, soup, HOST, USER, PW, DB, numQuestions
+	print "Parsing %s" % cat
+	num_parsed = 0
+	fname = "data/%s.json" % cat
+	f = open(fname, 'r')
+	err = open("errors.txt", "a");
+	err.write("Time of Crawl: %s\n" % strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+	fData = f.read()
+	jObj = json.loads(fData)
+	min_nums = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
+	num_blank = 0
+	conn = httplib.HTTPConnection("answers.yahoo.com:80")
+	sqlCon = None
+	count = 1
+	try:
+		sqlCon = _mysql.connect(HOST, USER, PW, DB)
+		for result in jObj:
+			if result['query']['results'] is None:
+				print "Blank question set"
+				num_blank += 1
+			else:
+				for q in result['query']['results']['Question']:
+					try:
+						#check if question has 5 or more answers
+						for key in min_nums.keys():
+							if(int(q['NumAnswers']) >= key):
+								min_nums[key] += 1
+						if(int(q['NumAnswers']) >= 5):
+							print "Processing question %d" % (count)
+							count += 1
+							print "Storing question: %s" % q['id']
+							#Store question into database
+							#Please excuse the following code, I am relatively new to Python and did not know of a nice way to prepare MySQL statments... will hopefully clean
+							d = {}
+							d['id'] = _mysql.escape_string(q['id'])
+							d['type'] = _mysql.escape_string(q['type'])
+							d['subject'] = _mysql.escape_string(q['Subject'].encode('utf-8'))
+							d['content'] = _mysql.escape_string(q['Content'].encode('utf-8'))
+							d['date'] = _mysql.escape_string(q['Date'])
+							d['specific_category'] = _mysql.escape_string(q['Category']['content'].encode('utf-8'))
+							d['category_id'] = _mysql.escape_string(q['Category']['id'])
+							d['user_id'] = _mysql.escape_string(q['UserId'])
+							d['user_nickname'] = "Not Specified"
+							if q['UserNick'] is not None:
+								d['user_nickname'] =_mysql.escape_string(q['UserNick'].encode('utf-8'))
+							d['num_answers'] = int(q['NumAnswers'])
+							d['num_comments'] = int(q['NumComments'])
+							d['chosen_answerer_id'] = ""
+							d['chosen_answer_timestamp'] = "NULL" 
+							d['chosen_answer_award_timestamp'] = "NULL"
+							if q['ChosenAnswererId'] is not None:
+								d['chosen_answerer_id'] = _mysql.escape_string(q['ChosenAnswererId'])
+								d['chosen_answer_timestamp'] = "FROM_UNIXTIME(%d)" % int(q['ChosenAnswerTimestamp'])
+								d['chosen_answer_award_timestamp'] = "FROM_UNIXTIME(%d)" % int(q['ChosenAnswerAwardTimestamp'])
+							d['category'] = cat
+							sqlCon.query("INSERT INTO question (`id`, `type`, `subject`, `content`, `datetime`, `specific_category`, `category_id`, `user_id`, `user_nickname`, `num_answers`, `num_comments`, `chosen_answer_id`, `chosen_answer_timestamp`, `chosen_answer_award_timestamp`, `category`) VALUES(\"%s\", \"%s\", \"%s\",\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", %d, %d, \"%s\", %s, %s, \"%s\")" % (d['id'],d['type'],d['subject'],d['content'],d['date'],d['specific_category'],d['category_id'],d['user_id'],d['user_nickname'],d['num_answers'],d['num_comments'],d['chosen_answerer_id'],d['chosen_answer_timestamp'],d['chosen_answer_award_timestamp'], d['category']))
+
+							#fetch page and scrape
+							#20100425114216AA802rx
+							conn.request("GET", "/question/index?qid=%s" % q['id'])
+							r = conn.getresponse()
+							data = r.read()
+							soup = BeautifulSoup(data)
+							#get all answers on this page
+
+							#best answer has class 'answer best'
+							#other answers just have class 'answer'
+							answers = soup.select(".answer")
+							best = soup.select(".answer.best")
+							hasBest = False
+							if len(best) == 1:
+								hasBest = True
+
+							for ans in answers:
+								#get each attribute
+								isBest = 0
+								if hasBest and ans == best[0]:
+									isBest = 1
+
+								content = ans.find_all("div", class_="content")[0].get_text()
+								upvotes = ans.find_all("li", class_="rate-up")
+								time_posted = ans.find_all("abbr")[0]['title']
+
+								
+								if len(upvotes) == 1:
+									#use regex to read the number of votes
+									s = upvotes[0].span.get_text()
+									m = re.compile("^[0-9]+").match(s)
+									upvotes = int(m.group(0))
+								else:
+									upvotes = 0
+
+								sqlCon.query("INSERT INTO answer (`qid`, `is_best`, `time_posted`, `upvotes`, `content`) VALUES(\"%s\", %d, \"%s\", %d, \"%s\")" % (_mysql.escape_string(q['id']), isBest, time_posted, upvotes, _mysql.escape_string(content.encode('utf-8'))))
+								#Yahoo only shows downvotes when signed in... maybe I will consider trying
+								#print isBest, upvotes, time_posted, content[0:10]
+							num_parsed += 1
+							#return
+					except _mysql.Error, e:
+						err.write("Error %d: %s" % (e.args[0], e.args[1]))
+						err.write("\n")
+						print "MySQL error occured"
+	except _mysql.Error, e:
+		print "Error %d: %s" % (e.args[0], e.args[1])
+		sys.exit(1)
+	finally:
+		if sqlCon:
+			sqlCon.close()
+	conn.close()
+	err.close()
+	f.close()
+	print "Finished"
+	print "%d blank sets" % num_blank
+	for key in min_nums.keys():
+		print "%d or more : %d" % (key, min_nums[key])
+
+
+#fetchData("Business & Finance")
+#parseData("Business & Finance")
+
+#fetchData("Health")
+#parseData("Health")
+
+#fetchData("Travel")
+#parseData("Travel")
+
+#fetchData("Sports")
+#parseData("Sports")
+
+#fetchData("Home & Garden")
+#parseData("Home & Garden")
+
+#fetchData("Entertainment & Music")
+#parseData("Entertainment & Music")
+
